ce70018aa94bed376a4b6211c5d0f53b44472d9e
sam-spain <sam@samspain.com>
2021-07-02 12:55:19 +0100

    Hello from controller

--- src/main/java/com/samspain/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/com/samspain/springpetclinic/SpringPetClinicApplication.java
@@ -1,13 +1,19 @@
 package com.samspain.springpetclinic;
 
+import com.samspain.springpetclinic.controllers.MyController;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPetClinicApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringPetClinicApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
